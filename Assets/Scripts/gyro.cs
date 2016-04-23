5e1cea34efc697bd973b348566ba7517661ec421
jinze yu <jinze.yu.kp@gmail.com>
2016-04-23 23:52:03 +0900

    navigation system and map

---
+++ Assets/Scripts/gyro.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gyro : MonoBehaviour
+{
+    private Vector2 velocity = new Vector2(0.0f, 0.0f);
+#if UNITY_EDITOR
+    private Vector3 rot;
+#endif
+
+    // Use this for initialization
+    void Start()
+    {
+        // debug mode
+        for (int i = 0; i < 100; ++i)
+        {
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            go.transform.position = Random.onUnitSphere * 10f;
+            go.transform.rotation = Random.rotation;
+        }
+#if UNITY_EDITOR
+        rot = transform.rotation.eulerAngles;
+#else
+        Input.gyro.enabled = true;
+#endif
+    }
+    void ModSpeed(Vector2 a)
+    {
+        velocity += a;
+    }
+    void ModAngle()
+    {
+#if UNITY_EDITOR
+        float spd = Time.deltaTime * 100.0f;
+        rot += new Vector3(-spd * Input.GetAxis("Mouse Y"), spd * Input.GetAxis("Mouse X"), 0.0f);
+        transform.rotation = Quaternion.Euler(rot);
+#else
+        transform.rotation = Quaternion.AngleAxis(90.0f,Vector3.right)*Input.gyro.attitude*Quaternion.AngleAxis(180.0f,Vector3.forward);
+#endif
+    }
+    void Slower()
+    {
+        if (velocity.magnitude < 0.5)
+        {
+            velocity.x = 0.0f;
+            velocity.y = 0.0f;
+        }
+        else
+        {
+            velocity *= 0.9f;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ModAngle();
+        ModSpeed(new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")));
+        //transform.Translate(velocity.x * Vector3.forward * Time.deltaTime + velocity.y * Vector3.right * Time.deltaTime);
+        //Slower();
+    }
+}
---
+++ Assets/Scripts/playermove.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using CnControls;
+using System.Collections;
+
+// This is merely an example, it's for an example purpose only
+// Your game WILL require a custom controller scripts, there's just no generic character control systems, 
+// they at least depend on the animations
+
+[RequireComponent(typeof(CharacterController))]
+public class playermove : MonoBehaviour
+{
+    public float MovementSpeed = 10f;
+
+    private Transform _mainCameraTransform;
+    private Transform _transform;
+    private CharacterController _characterController;
+	private Vector3 firstPos = new Vector3(0f, 0f, 0f);
+	private Vector3 firstRo = new Vector3 (10f, 0f, 0f);
+	private Vector3 thirdPos = new Vector3(0f, 1.5f, -13f);
+	private Vector3 thirdRo = new Vector3(30f, 0f, 0f);
+	private int changePer = 0;
+
+    private void OnEnable()
+    {
+		_mainCameraTransform = GameObject.FindWithTag("MainCamera").GetComponent<Transform>();
+        _characterController = GetComponent<CharacterController>();
+        _transform = GetComponent<Transform>();
+    }
+
+    public void Update()
+    {
+		if (Input.GetKeyDown("k")) {
+			GameObject gameobject = GameObject.FindWithTag ("MainCamera");
+			if (changePer == 0) {
+				//Vector3 newPos = new Vector3(gameObject.GetComponent<Transform> ().position.x, gameObject.GetComponent<Transform> ().position.y+1.5f, gameObject.GetComponent<Transform> ().position.z-13f);
+				//gameObject.GetComponent<Transform> ().position = newPos;
+				//Quaternion newRo = Quaternion.Euler(gameObject.GetComponent<Transform> ().rotation.x-10, 0, 0);
+				//gameObject.GetComponent<Transform> ().localRotation = newRo;
+				gameobject = GameObject.Find("firstcamera");
+				changePer = 1;
+			} else {
+				//Vector3 newPos = new Vector3(gameObject.GetComponent<Transform> ().position.x, gameObject.GetComponent<Transform> ().position.y-1.5f, gameObject.GetComponent<Transform> ().position.z+13f);
+				//gameObject.GetComponent<Transform> ().position = newPos;
+				//Quaternion newRo = Quaternion.Euler(gameObject.GetComponent<Transform> ().rotation.x+10, 0, 0);
+				//gameObject.GetComponent<Transform> ().localRotation = newRo;
+				gameobject = GameObject.Find("thirdcamera");
+				changePer = 0;
+			}
+
+		}
+		//Debug.Log (_mainCameraTransform.position.x + ", " + _mainCameraTransform.position.y);
+        // Just use CnInputManager. instead of Input. and you're good to go
+        var inputVector = new Vector3(CnInputManager.GetAxis("Horizontal"), CnInputManager.GetAxis("Vertical"));
+        Vector3 movementVector = Vector3.zero;
+
+        // If we have some input
+        if (inputVector.sqrMagnitude > 0.001f)
+        {
+            movementVector = _mainCameraTransform.TransformDirection(inputVector);
+            movementVector.y = 0f;
+            movementVector.Normalize();
+            _transform.forward = movementVector;
+        }
+
+		//Debug.Log (movementVector.x + ", " + movementVector.y);
+        //movementVector += Physics.gravity;
+        _characterController.Move(movementVector * Time.deltaTime* 200);
+    }
+}
